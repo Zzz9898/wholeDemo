01c09cf21a8ec6bb5f0160fa6271c0f86f754335
zhangjw <1617386850@qq.com>
2020-11-18 15:11:42 +0800

    init att project

---
+++ whole-demo/attempt-demo/src/main/java/com/zzz/att/AttemptDemoApplication.java
@@ -0,0 +1,11 @@
+package com.zzz.att;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttemptDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AttemptDemoApplication.class);
+    }
+}
